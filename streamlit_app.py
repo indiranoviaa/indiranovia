ac9069f613f1957b4e08e58054ffa624dfd34e11
indiranoviaa <indiranovianti11@gmail.com>
2025-05-15 14:47:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("🎈 indira's ")
-st.write(" welcome y'all " )
+st.write(" welcome y'all 
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
